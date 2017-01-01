3e87041610b93aea305119634fc1a98c0d6a4610
Ewout van der Linden <IonKiwi@users.noreply.github.com>
2017-01-01 20:02:17 +0100

    Rebrand to lineage

--- lineage.mk
+++ lineage.mk
@@ -20,7 +20,7 @@ $(call inherit-product, device/samsung/kccat6xx/full_kccat6xx.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := kccat6xx
-PRODUCT_NAME := cm_kccat6xx
+PRODUCT_NAME := lineage_kccat6xx
 
 # SH LANGUAGE SET
 PRODUCT_DEFAULT_LANGUAGE := en
