07c756c6727cdcf7181ed02217a9cf4c33a3926b
Corinna Vinschen <xda@vinschen.de>
2017-03-30 19:40:03 +0200

    kccat6: Remove explicit language settings
    Change-Id: I936b8cf1723a57bb9f20b033ce89fed0477cd0f7
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- lineage.mk
+++ lineage.mk
@@ -22,10 +22,3 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := kccat6
 PRODUCT_NAME := lineage_kccat6
-
-# SH LANGUAGE SET
-PRODUCT_DEFAULT_LANGUAGE := en
-PRODUCT_DEFAULT_REGION := US
-
-PRODUCT_LOCALES := en_US ko_KR en_GB en_CA en_AU en_NZ en_SG zh_CN zh_TW ja_JP fr_FR fr_BE fr_CA fr_CH it_IT it_CH es_ES de_DE de_AT de_CH de_LI nl_NL nl_BE cs_CZ pl_PL ru_RU # vi_VN
-
