a7c2087cbfb2549b7592b698a267ad832eca2312
Corinna Vinschen <xda@vinschen.de>
2018-04-13 13:39:39 +0200

    kccat6: Accommodate ResurrectionRemix build
    Change-Id: I2fd7658a8a879c30ee1c7b268b14311688b04b51
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

---
+++ rr.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2016 The Sayanogen Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, device/samsung/kccat6/full_kccat6.mk)
+
+# Inherit common RR phone.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+
+PRODUCT_DEVICE := kccat6
+PRODUCT_NAME := rr_kccat6
